157c13032287065575726d26ee6945f38db6bb96
José Miguel Santos <jmcpsantos@gmail.com>
2024-04-18 16:41:21 +0100

    Ficha 13

---
+++ src/Ex_03/Person.java
@@ -0,0 +1,26 @@
+package Ex_03;
+
+public abstract class Person {
+    protected String name;
+    protected int yearOfBirth;
+    protected String email;
+    protected String phone;
+
+    public Person(String name, int yearOfBirth, String email, String phone) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public abstract void jobTitle();
+
+    public abstract void printTimetable();
+
+    public String personDetails() {
+        return "Nome: " + name +
+                " | Ano de Nascimento: " + yearOfBirth +
+                " | Email: " + email +
+                " | Telefone: " + phone;
+    }
+}
---
+++ src/Ex_03/Teacher.java
@@ -0,0 +1,20 @@
+package Ex_03;
+
+import java.util.ArrayList;
+
+public class Teacher extends Person{
+    //private ArrayList<TrainingArea> trainingArea;
+    public Teacher(String name, int yearOfBirth, String email, String phone) {
+        super(name, yearOfBirth, email, phone);
+    }
+
+    @Override
+    public void jobTitle() {
+
+    }
+
+    @Override
+    public void printTimetable() {
+
+    }
+}
